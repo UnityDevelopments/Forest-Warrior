0cbb5e0b375107c85743b781149ac99de796bbee
Dapro001e21 <Dapro004@gmail.com>
2023-02-13 18:52:48 +0300

    commit

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -4,140 +4,62 @@ using UnityEngine;
 
 public class PlayerControls : MonoBehaviour
 {
-    GameObject Soldier_demo;
-    GameObject playecontrol;
-    GameObject[] enemies;
-    public float horizontalSpeed = 2.0f;
-    public float vertical;
-    public float horisontal;
-    public float moveAmount;
     public float speed = 5f;
     public float speedRotation = 3f;
     public bool isGround;
-    //камера
-    public Transform targetRotate;
-    public Animator anim;
-    CharacterController characterController;
-    /*public AnimationClip animation;*/ //Анимация
-    private Rigidbody _rb;
-    //переменная удара
-    const float ComboTime = 0.2f;
-    float m_LastClickTime;
-    public float Gravity = 9.8f;
-    public float GravityScale = 1;
-    public float velocity = 5;
-    public float jumpHeight = 4;
     public float jumpSpeed;
     private Rigidbody rb;
+    private Animator anim;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        // Обьявление героя
-        Soldier_demo = gameObject;
-        //enemies = GameObject.FindGameObjectsWithTag("Enemy");
-        _rb = GetComponent<Rigidbody>();
-        anim = GetComponent<Animator>();
-        //задержка 3 сек
-        anim.SetTrigger("Idle");
         rb = GetComponent<Rigidbody>();
-
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float x = Input.GetAxis("Horizontal");
-        float z = Input.GetAxis("Vertical");
 
-        anim.SetFloat("Walk", z);
-        anim.SetFloat("Rotate", x);
+        if(Input.GetKey(KeyCode.W))
+        {
+            anim.SetBool("IsRunning", true);
+        }
+        else
+        {
+            anim.SetBool("IsRunning", false);
+        }
 
         //Движение вперед
         if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
         {
-            Soldier_demo.transform.position += Soldier_demo.transform.forward * speed * Time.deltaTime;
+            gameObject.transform.position += gameObject.transform.forward * speed * Time.deltaTime;
         }
         //Движение назад
         if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
         {
-            Soldier_demo.transform.position -= Soldier_demo.transform.forward * speed * Time.deltaTime;
+            gameObject.transform.position -= gameObject.transform.forward * speed * Time.deltaTime;
         }
         //Движение вправо
         if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
         {
-            Soldier_demo.transform.Rotate(Vector3.up * speedRotation);
+            gameObject.transform.Rotate(Vector3.up * speedRotation);
 
         }
         //Движение влево
         if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
         {
-            Soldier_demo.transform.Rotate(Vector3.down * speedRotation);
+            gameObject.transform.Rotate(Vector3.down * speedRotation);
         }
 
-        if (Input.GetMouseButtonDown(0))
-        {
-            if (Time.time - m_LastClickTime >= ComboTime)
-            {
-                anim.SetBool("Attacke", true);
-            }
-            else
-            {
-                anim.SetTrigger("Idle");
-            }
-            m_LastClickTime = Time.time;
-        }
         //Движение вверх
         if (Input.GetKeyDown(KeyCode.Space) && isGround)
         {
             rb.AddForce(transform.up * jumpSpeed);
         }
     }
-
-    //void MovePlayer()
-    //{
-    //    characterController.Move(new Vector3(0, velocity, 0) * Time.deltaTime);
-    //}
-    //button
-    //void Button()
-    //{
-    //    GameObject newButton = new GameObject("New button", typeof(Image), typeof(Button));
-    //}
-
-    ////физика обьекта
-    //void FixedUpdate()
-    //{
-    //   // MovementLogic();
-    //    JumpLogic();
-    //    if (Input.GetMouseButtonUp(0))
-    //    {
-    //        if (timer <= 0.01f)
-    //        {
-    //            if(Attacke != 1)
-    //            {
-    //                Attacke += 1;
-    //            }
-    //            else
-    //            {
-    //                Attacke = 0;
-    //            }
-    //            Vector3 dir = targetRotate.position - transform.position;
-    //            dir.y = 0;
-    //            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(dir), 40 * Time.deltaTime);
-    //        }
-    //    }
-    //}
-
-    //private void MovementLogic()
-    //{
-    //    vertical = Input.GetAxis("Vertical");
-    //    horisontal = Input.GetAxis("Horizontal");
-    //    moveAmount = Mathf.Clamp01(Mathf.Abs(vertical) + Mathf.Abs(horisontal));
-
-    //    anim.SetFloat("Red_Gnome_walk",vertical,0.15f, Time.deltaTime);
-    //}
-
 
 
     private void OnCollisionStay()
@@ -157,9 +79,4 @@ public class PlayerControls : MonoBehaviour
             isGround = value;
         }
     }
-    ////использование анимации
-    //private void Anim()
-    //{
-
-    //}
 }
