46dfae364b441aa6d8dbde4220aa2619108601b6
AlexNub <patkov01@mail.ru>
2023-02-18 17:00:49 +0200

    1
    1

---
+++ Assets/Scripts/AI/MobsAI.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.AI;
+using UnityEngine;
+
+public class MobsAI : MonoBehaviour
+{
+    private NavMeshAgent navMeshAgent;
+    [SerializeField] private float changePositionTime = 5f;
+    [SerializeField] private float moveDistance = 10f;
+
+    private void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        InvokeRepeating(nameof(RandomNavSphere), changePositionTime, repeatRate: changePositionTime);
+    }
+    
+
+    Vector3 RandomNavSphere(float distance)
+    {
+        Vector3 randomDirection = UnityEngine.Random.insideUnitSphere * distance;
+
+        randomDirection += transform.position;
+
+        NavMeshHit navHit;
+
+        NavMesh.SamplePosition(randomDirection, out navHit, distance, -1);
+
+        return navHit.position;
+    }
+
+    private void MoveAnimal()
+    {
+        navMeshAgent.SetDestination(RandomNavSphere(moveDistance));
+    }
+}
