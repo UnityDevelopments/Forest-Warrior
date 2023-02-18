e18b951b5b787b197324dfca9f1b1909031cd239
Dapro001e21 <Dapro004@gmail.com>
2023-02-18 17:34:31 +0300

    commit

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -1,86 +1,93 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerControls : MonoBehaviour
 {
-    public float speed = 5f;
-    public float speedRotation = 3f;
-    public float acceleration;
-    public bool isGround;
-    public float jumpSpeed;
-    public GameObject rotateObject;
-    private Rigidbody rb;
-    private Animator anim;
-
+    public float Speed = 5f;
+    public float SpeedRotation = 3f;
+    public float Hp;
+    public float Acceleration;
+    public bool IsGround;
+    public float JumpSpeed;
+    public GameObject RotateObject;
+    public Slider HpSlider;
+    private Rigidbody Rb;
+    private Animator Anim;
 
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
-        anim = GetComponent<Animator>();
+        Rb = GetComponent<Rigidbody>();
+        Anim = GetComponent<Animator>();
+        HpSlider.value = Hp / 100;
     }
 
     // Update is called once per frame
     void Update()
     {
+        //Обновление жизней
+        HpSlider.value = Hp / 100;
 
         //Движение вперед
-        if (Input.GetKey(KeyCode.W) && isGround)
+        if (Input.GetKey(KeyCode.W) && IsGround)
         {
-            anim.SetBool("IsRunning", true);
+            Anim.SetBool("IsRunning", true);
             if (Input.GetKey(KeyCode.LeftShift))
             {
-                gameObject.transform.position += gameObject.transform.forward * speed * acceleration * Time.deltaTime;
+                Anim.SetFloat("SpeedRun", 2);
+                gameObject.transform.position += gameObject.transform.forward * Speed * Acceleration * Time.deltaTime;
             }
             else
             {
-                gameObject.transform.position += gameObject.transform.forward * speed * Time.deltaTime;
+                Anim.SetFloat("SpeedRun", 1);
+                gameObject.transform.position += gameObject.transform.forward * Speed * Time.deltaTime;
             }
         }else
         {
-            anim.SetBool("IsRunning", false);
+            Anim.SetBool("IsRunning", false);
         }
 
 
         //Движение назад
         if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
         {
-            gameObject.transform.position -= gameObject.transform.forward * speed * Time.deltaTime;
+            gameObject.transform.position -= gameObject.transform.forward * Speed * Time.deltaTime;
         }
         //Движение вправо
         if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
         {
-            gameObject.transform.Rotate(Vector3.up * speedRotation);
+            gameObject.transform.Rotate(Vector3.up * SpeedRotation);
 
         }
         //Движение влево
         if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
         {
-            gameObject.transform.Rotate(-Vector3.up * speedRotation);
+            gameObject.transform.Rotate(-Vector3.up * SpeedRotation);
         }
 
         //Движение вверх
-        if (Input.GetKeyDown(KeyCode.Space) && isGround)
+        if (Input.GetKeyDown(KeyCode.Space) && IsGround)
         {
-            rb.AddForce(transform.up * jumpSpeed);
+            Rb.AddForce(transform.up * JumpSpeed);
         }
 
         //Атака
         if (Input.GetMouseButtonDown(0))
         {
-            anim.SetBool("IsAttack", true);
+            Anim.SetBool("IsAttack", true);
         }
         else
         {
-            anim.SetBool("IsAttack", false);
+            Anim.SetBool("IsAttack", false);
         }
     }
 
 
     private void OnCollisionStay()
     {
-        isGround = true;
+        IsGround = true;
     }
 
     void OnCollisionExit(Collision collision)
@@ -92,7 +99,7 @@ public class PlayerControls : MonoBehaviour
     {
         if (collision.gameObject.tag == ("Ground"))
         {
-            isGround = value;
+            IsGround = value;
         }
     }
 }
