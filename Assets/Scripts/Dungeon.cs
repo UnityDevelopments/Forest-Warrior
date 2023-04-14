f0b52b116aee38d3b4fc18f8f745558a8b628c09
Dapro001e21 <Dapro004@gmail.com>
2023-04-14 22:27:36 +0300

    commit

---
+++ Assets/Scripts/Dungeon.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Dungeon : MonoBehaviour
+{
+    private MyButton openButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        openButton = GameObject.Find("OpenButton").GetComponent<MyButton>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            openButton.gameObject.GetComponent<Image>().enabled = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            openButton.gameObject.GetComponent<Image>().enabled = false;
+        }
+    }
+}
